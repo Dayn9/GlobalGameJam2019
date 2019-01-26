89af31fc32a8820a2b7104eaa5e1895720dc1b35
Jared Feeley (RIT Student) <jf3925@ad.rit.edu>
2019-01-26 15:13:33 -0500

    Throwing mechanic taking form

--- SockToss/Assets/Throwable.cs
+++ SockToss/Assets/Throwable.cs
@@ -7,17 +7,38 @@ public class Throwable : MonoBehaviour {
     [SerializeField]
     private float followSpeed;
 
+    [SerializeField]
+    private float endForce;
+
+    [SerializeField]
+    private float magMultiplier;
+
     private bool isMoving;
+
+    private Rigidbody rb;
+    private Touch touch;
 
 	// Use this for initialization
 	void Start () {
         isMoving = false;
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
 	}
+
+    class MouseTouch {
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Touch touch = Input.GetTouch(0);
+        if(Input.touches.Length != 0)
+            touch = Input.GetTouch(0);
+
         if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0.55f));
+            rb.velocity = new Vector3(0,0,0);
+            rb.angularVelocity = new Vector3(0, 0, 0);
+
             Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             RaycastHit raycastHit;
             if (Physics.Raycast(raycast, out raycastHit)) {
@@ -27,11 +48,18 @@ public class Throwable : MonoBehaviour {
             }
         }
 
-        if(isMoving) {
+        
+    }
+
+    void FixedUpdate() {
+        if (isMoving){
             Vector3 touchedPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, transform.position.z));
             transform.position = Vector3.Lerp(transform.position, touchedPos, Time.deltaTime * followSpeed);
-            if (touch.phase == TouchPhase.Ended){
+            if (touch.phase == TouchPhase.Ended) {
+                Vector3 swipe = touch.deltaPosition;
                 isMoving = false;
+                rb.isKinematic = false;
+                rb.AddForce(new Vector3(swipe.x, swipe.y, touch.deltaPosition.magnitude) * magMultiplier, ForceMode.Impulse);
             }
         }
     }
