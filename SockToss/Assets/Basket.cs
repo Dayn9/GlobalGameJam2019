47aae0ab8474049bfb9c3ea12bab2ca940f01ce7
Jared Feeley (RIT Student) <jf3925@ad.rit.edu>
2019-01-26 18:08:55 -0500

    Object pool implemented for SockSpawner

---
+++ SockToss/Assets/Basket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basket : MonoBehaviour {
+    [SerializeField]
+    private Text scoreText;
+
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "Score: " + score;
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Throwable") {
+            score++;
+        }
+    }
+}
--- SockToss/Assets/SockSpawner.cs
+++ SockToss/Assets/SockSpawner.cs
@@ -6,11 +6,16 @@ public class SockSpawner : MonoBehaviour {
     [SerializeField]
     private GameObject sock;
 
+    [SerializeField]
+    private int poolSize;
+
+    private Queue<GameObject> pool;
     private bool touching;
     private Touch touch;
 
 	// Use this for initialization
 	void Start () {
+        pool = new Queue<GameObject>();
         touching = false;
 	}
 	
@@ -19,7 +24,12 @@ public class SockSpawner : MonoBehaviour {
         if (Input.touches.Length != 0) {
             touch = Input.GetTouch(0);
             if (!touching) {
-                Instantiate(sock, Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0.55f)), Quaternion.identity);
+                if(pool.Count < poolSize)
+                    pool.Enqueue(Instantiate(sock, Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0.55f)), new Quaternion(0,90,0,90)));
+                else {
+                    Destroy(pool.Dequeue());
+                    pool.Enqueue(Instantiate(sock, Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 0.55f)), new Quaternion(0, 90, 0, 90)));
+                }
                 touching = true;
             } else if(touch.phase == TouchPhase.Ended)
             touching = false;
