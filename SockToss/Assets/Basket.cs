a25be39cb1ba119a6003a3c455482e79b3ba2640
Jared Feeley (RIT Student) <jf3925@ad.rit.edu>
2019-01-27 11:46:35 -0500

    Basket position randomization implemented

--- SockToss/Assets/Basket.cs
+++ SockToss/Assets/Basket.cs
@@ -7,23 +7,33 @@ public class Basket : MonoBehaviour {
     [SerializeField]
     private Text scoreText;
 
+    [SerializeField]
+    private Transform[] basketPositions;
+
     private int score;
     private AudioSource scoreSound;
 
     // Use this for initialization
     void Start () {
         scoreSound = GetComponent<AudioSource>();
+        Transform tBucket = basketPositions[Random.Range(0, basketPositions.Length)];
+        transform.position = tBucket.position;
+        transform.rotation = tBucket.rotation;
     }
 	
 	// Update is called once per frame
 	void Update () {
         scoreText.text = "Score: " + score;
-	}
+    }
 
     private void OnTriggerEnter(Collider other) {
         if(other.tag == "Throwable") {
             score++;
             scoreSound.Play(0);
+            Transform tBucket = basketPositions[Random.Range(0, basketPositions.Length)];
+            transform.position = tBucket.position;
+            transform.rotation = tBucket.rotation;
+            Destroy(other.gameObject);
         }
     }
 }
